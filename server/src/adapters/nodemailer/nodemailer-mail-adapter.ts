3d806fe1350af952d63e28a78aaeca829cc6c820
Rodrigo Nunes <imrodrigon@gmail.com>
2022-05-06 16:15:47 -0400

    backend

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from '../mail-adapter';
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'c9f0aaabbf96a7',
+    pass: '9c73322e971e9a',
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Rodrigo Nunes <imrodrigon@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
